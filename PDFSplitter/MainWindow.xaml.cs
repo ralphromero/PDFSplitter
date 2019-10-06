3f8de573ec6f56c8ac70b6ae95c16d67b25a1baf
Ralph Romero <ralph@romero.id.au>
2019-10-06 23:15:29 +1030

    Add project files.

---
+++ PDFSplitter/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using PDFSplitter.Core;
+using System.Text;
+using System.Windows;
+
+namespace PDFSplitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            System.Text.Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            InitializeComponent();
+        }
+
+        private void FileChooseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dog = new OpenFileDialog();
+            dog.Filter = "PDF Files|*.pdf";
+            if (dog.ShowDialog() == true)
+            {
+                FileChosen.Text = dog.FileName;
+            }
+        }
+
+        private void GenerateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var pdf = new PDFTextExtractor();
+            pdf.SplitPDF(FileChosen.Text, SearchExpr.Text);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# PDF Splitter
+This will split a PDF file into multiple smaller PDF files based on a regular expression search.
+
+## How to Use
+Build and run the program. Give the path to your PDF. Put in a regular expression. You can also just treat this as a string search. Click generate and the files will be output to the same folder as the source PDF.
